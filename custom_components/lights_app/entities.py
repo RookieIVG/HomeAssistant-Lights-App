f4af381d2d4ad061b5e564d280904a47b3ebcfaa
RookieIVG <93792760+RookieIVG@users.noreply.github.com>
2026-01-12 19:50:29 +0100

    Refactor LightsAppEntity initialization and properties

--- custom_components/lights_app/entities.py
+++ custom_components/lights_app/entities.py
@@ -1,33 +1,35 @@
 from homeassistant.util import slugify
 from homeassistant.helpers.entity import DeviceInfo, Entity
-from homeassistant.const import CONF_ADDRESS, STATE_UNAVAILABLE
-from homeassistant.components.light import LightEntity
-from homeassistant.components.switch import SwitchEntity
-from homeassistant.components.light import ColorMode
+from homeassistant.const import CONF_ADDRESS
+from homeassistant.components.light import LightEntity, ColorMode
 from .const import DOMAIN
 
-
 class LightsAppEntity(Entity):
-    def __init__(self, hass, config_entry: dict, entryData: dict, name_suffix: str):
+    def __init__(self, hass, config_entry, entryData, name_suffix):
         self._hass = hass
         self._address = config_entry.data.get(CONF_ADDRESS)
-        self._client = hass.data[DOMAIN][config_entry.entry_id]["connection"]["client"]
-        self._service = hass.data[DOMAIN][config_entry.entry_id]["connection"][
-            "service"
-        ]
         self._entryData = entryData
         self._config_entry = config_entry
-        self._enabled = False
         self._name = "Lights App"
         self._name_suffix = name_suffix
+        super().__init__()
 
-        self._attr_color_mode = ColorMode.ONOFF
-        self._attr_supported_color_modes = set([ColorMode.ONOFF])
-        Entity.__init__(self)
+    @property
+    def _client(self):
+        return self._hass.data[DOMAIN][self._config_entry.entry_id]["connection"].get("client")
+
+    @property
+    def _service(self):
+        return self._hass.data[DOMAIN][self._config_entry.entry_id]["connection"].get("service")
+
+    @property
+    def available(self) -> bool:
+        client = self._client
+        return client is not None and client.is_connected
 
     @property
     def name(self) -> str:
-        return "{} {}".format(self._name, self._name_suffix)
+        return f"{self._name} {self._name_suffix}"
 
     @property
     def device_info(self) -> DeviceInfo:
@@ -40,17 +42,10 @@ class LightsAppEntity(Entity):
 
     @property
     def unique_id(self) -> str:
-        id_suffix = "".join(self._name_suffix.split())
-        return "{}-{}-{}".format(self._address, self._name, id_suffix).lower()
-
+        id_suffix = "".join(self._name_suffix.split()).lower()
+        return f"{self._address}-{self._name}-{id_suffix}".lower()
 
 class LightsAppLightEntity(LightEntity, LightsAppEntity):
-    def __init__(self, hass, config_entry: dict, entryData: dict, name_suffix: str):
+    """Basis für alle Licht-Typen."""
+    def __init__(self, hass, config_entry, entryData, name_suffix):
         LightsAppEntity.__init__(self, hass, config_entry, entryData, name_suffix)
-        LightEntity.__init__(self)
-
-
-class LightsAppSwitchEntity(SwitchEntity, LightsAppEntity):
-    def __init__(self, hass, config_entry: dict, entryData: dict, name_suffix: str):
-        LightsAppEntity.__init__(self, hass, config_entry, entryData, name_suffix)
-        SwitchEntity.__init__(self)
